acfb5d056a92f3c609c6fd7e0f6a62786feef223
duhuan <duhuan>
2022-08-09 17:42:03 +0800

    router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../page/home/index'
+import Login from '../page/home/login'
+
+const routes = [
+  {
+    path: '/',
+    beforeEnter: (to, from, next) => {
+      if (to.path === '/') {
+        next('/home/loge')
+      } else next()
+    }
+  }, {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    children: [
+      {
+        path: 'login',
+        component:Login
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  base: process.env.BASE_URL,
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
